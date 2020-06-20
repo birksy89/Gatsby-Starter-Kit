46698137709e7488fbe9bbd7ebf842227e3ea07b
Andrew <andrew@bbm.co.uk>
2020-06-20 10:00:11 +0100

    react-🔥-dom patch is not detected

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,18 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+// react-🔥-dom patch is not detected. React 16.6+ features may not work
+//  https://stackoverflow.com/a/56093675/3461845
+//  https://github.com/gatsbyjs/gatsby/issues/11934#issuecomment-469046186
+//  https://stackoverflow.com/a/62221716/3461845
+exports.onCreateWebpackConfig = ({ stage, actions }) => {
+  if (stage.startsWith("develop")) {
+    actions.setWebpackConfig({
+      resolve: {
+        alias: {
+          "react-dom": "@hot-loader/react-dom",
+        },
+      },
+    })
+  }
+}
